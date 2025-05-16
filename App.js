e585b42b95707b7abe36a6046036a40e52f67812
mimansha19 <mimansha.agarwal.met21@itbhu.ac.in>
2025-05-16 18:05:03 +0530

    Updated API URL to deployed backend

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [code, setCode] = useState('print("Hello, World!")');
-  const [input, setInput] = useState('');
-  const [output, setOutput] = useState('');
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
 
   const runCode = async () => {
-    const res = await fetch('http://localhost:5000/run', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ code, input })
+    const res = await fetch("https://code-editor-backend-h3k7.onrender.com", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code, input }),
     });
     const data = await res.json();
     setOutput(data.output);
@@ -19,8 +19,17 @@ function App() {
   return (
     <div className="App">
       <h2>Mini Python Code Editor</h2>
-      <textarea rows="10" value={code} onChange={(e) => setCode(e.target.value)} />
-      <textarea rows="5" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Input (if any)" />
+      <textarea
+        rows="10"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <textarea
+        rows="5"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Input (if any)"
+      />
       <button onClick={runCode}>Run</button>
       <pre>{output}</pre>
     </div>
